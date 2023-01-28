bc891611dfa397dc37b736c92d95d1c8b73533e0
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2023-01-28 23:31:47 +0530

    "implemented login Component"

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import HomepageComponent from "./Components/HomepageComponent";
 import LoginComponent from "./Components/LoginComponent";
 import RegisterComponent from "./Components/RegisterComponent";
 import VerificationComponent from "./Components/VerificationComponent";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<LoginComponent />} />
+        <Route path="/" element={<LoginComponent />} />
         <Route path="/register" element={<RegisterComponent />} />
         <Route path="/verification" element={<VerificationComponent />} />
         <Route path="/home" element={<HomepageComponent />} />
--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -1,7 +1,80 @@
-import React from "react";
+import { useState } from "react";
+import { Card, Button, Stack, Form } from "react-bootstrap";
 
 function LoginComponent() {
-  return <div>LoginComponent</div>;
+  const InitialLoginValues = {
+    email: "",
+    password: "",
+  };
+  const [loginValues, setLoginValues] = useState(InitialLoginValues);
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+  const changeHandler = (event) => {
+    const { name, value } = event.target;
+    setLoginValues({ ...loginValues, [name]: value });
+  };
+  return (
+    <div
+      class="container d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Card>
+        <Card.Header as="h3" className="text-bg-primary text-center">
+          Login
+        </Card.Header>
+        <Card.Body>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Floating className="mb-3">
+              <Form.Control
+                id="email"
+                name="email"
+                type="email"
+                placeholder="name@example.com"
+                required
+                value={loginValues.email}
+                onChange={changeHandler}
+              />
+              <Form.Label>Email</Form.Label>
+              <Form.Control.Feedback type="invalid">
+                Valid Email Required
+              </Form.Control.Feedback>
+            </Form.Floating>
+            <Form.Floating>
+              <Form.Control
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                value={loginValues.password}
+                onChange={changeHandler}
+              />
+              <Form.Label>Password</Form.Label>
+              <Form.Control.Feedback type="invalid">
+                Valid Password Required
+              </Form.Control.Feedback>
+            </Form.Floating>
+            <br />
+            <Stack direction="horizontal text-center" gap={3}>
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+              <Button variant="primary">Signup</Button>
+            </Stack>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default LoginComponent;
